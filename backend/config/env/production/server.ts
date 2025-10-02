fab9a6de33b15f8c8669e950532fa705a17e334e
Vishwas Naveen <vishwas.naveen2580@gmail.com>
2025-10-02 21:24:26 +0530

    refactor: apply proxy config directly to main server.ts

--- backend/config/env/production/server.ts
+++
@@ -1,9 +0,0 @@
-// path: backend/config/env/production/server.ts
-
-export default ({ env }) => ({
-  proxy: true,
-  url: env("RENDER_EXTERNAL_URL"),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-});
--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,13 @@
+// path: backend/config/server.ts
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true, // <-- We are forcing the proxy setting here
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
